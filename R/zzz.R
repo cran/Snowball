da669a8a0cbfb1450bc1abe0e804213150b93947
Kurt Hornik <Kurt.Hornik@R-project.org>
2009-08-30 00:00:00 +0000

    version 0.0-7

--- R/zzz.R
+++ R/zzz.R
@@ -1,17 +1,6 @@
 .onLoad <-
 function(libname, pkgname)
-{
-    ## Argh.  We do need RWeka's .onLoad() to be run so that the JVM
-    ## gets initialized properly ...
-    ## loadNamespace("RWeka")
-
-    ## <FIXME>
-    ## Add
-    ##   lib.loc = libname
-    ## when rJava adds the lib.loc argument.
-    rJava::.jpackage(pkgname)
-    ## </FIXME>
-}
+    rJava::.jpackage(pkgname, lib.loc = libname)
 
 SnowballStemmer <-
     RWeka:::make_Weka_stemmer("weka/core/stemmers/SnowballStemmer")
